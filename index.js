b68f81c25f5b8a93f64905e46ccc52ee503a7e62
grace-syingali <135760083+grace-syingali@users.noreply.github.com>
2023-06-20 09:11:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return(`This Saturday, I want to ${fun}!`);
+
+}
+function mondayWork(work = "go to the office") {
+    return(`This Monday, I will ${work}.`)
+}
+function wrapAdjective(wrap=""){
+
+    return(`You are ${wrap}successful${wrap}.`)
+
+}
